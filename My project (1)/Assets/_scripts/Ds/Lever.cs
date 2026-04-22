3584e478e0d77ae14bfdeb6cce019caa97fd0b2c
UnaTalKat <unatalkat.19@gmail.com>
2026-04-22 14:06:44 -0500

    Ultimos comentarios xd

--- My project (1)/Assets/_scripts/Ds/Lever.cs
+++ My project (1)/Assets/_scripts/Ds/Lever.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
+    // Referencia a la puerta que se abrirá
     public Door door;
-
+    // Fuente de audio
     public AudioSource audioSource;
+    // Sonido de la palanca
     public AudioClip leverSound;
-
+    // Indica si el jugador está dentro del rango de interacción
     private bool playerNearby = false;
+    // Evita que la palanca se use más de una vez
     private bool used = false;
 
     void Update()
     {
+        // Si el jugador está cerca, presiona E y no se ha usado aún
         if (playerNearby && Input.GetKeyDown(KeyCode.E) && !used)
         {
+            // Marca la palanca como usada
             used = true;
 
             //sonido de palanca
@@ -29,20 +34,22 @@ public class Lever : MonoBehaviour
             }
         }
     }
-
+    // Detecta cuando el jugador entra en el área de la palanca
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Permite interacción
             playerNearby = true;
             Debug.Log("Presiona E para usar palanca");
         }
     }
-
+    // Detecta cuando el jugador sale del área
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Desactiva interacción
             playerNearby = false;
         }
     }
--- My project (1)/Assets/_scripts/SO/Weapons.cs
+++ My project (1)/Assets/_scripts/SO/Weapons.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Permite crear este asset desde el menú de Unity
 [CreateAssetMenu(fileName = "newGun", menuName = "Weapon/New Weapon")]
 
+// Clase ScriptableObject que define los datos de un arma
 public class Weapons : ScriptableObject
 {
     [Tooltip("Distancia de disparo")]
+    //rango
     public float range;
+    // Altura del área de detección
     public int verticalRange;
+    // Anchura del área de detección
     public int horizontalRange;
+    // Tiempo entre disparos (cooldown)
     public float fireRate;
+    // Daño que hace cada disparo
     public int damage;
+    // Prefab visual del arma (modelo en mano)
     public GameObject weaponPrefab;
+    // Sonido del disparo
     public AudioClip sound;
 
     [Header("Ammo")]
     public int maxAmmo;      // cargador
     public int maxReserve;   // munición total extra
-    public float reloadTime;
+    public float reloadTime; // Tiempo que tarda en recargar
 
 }
